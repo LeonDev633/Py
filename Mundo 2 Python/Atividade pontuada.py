3b00c16cfeaa9e8ce3854e6da22c2fee459f5f02
LeonDev633 <leonardo.dev667622969@gmail.com>
2024-09-21 00:52:50 +0000

    Atividade pontuada

---
+++ Mundo 2 Python/Atividade pontuada.py
@@ -0,0 +1,84 @@
+# João Victor Agapio Modesto Mendes | Bruno Henrique Alves Santos | Leonardo Machado Brito | Nicolas Roger
+
+import os
+import random
+os.system ("cls || clear ")
+
+numeros_armazenados = []
+QTD = 5
+
+def logo_senai():
+    print("""\033[34m
+===============
+---  \033[37mSENAI\033[34m  ---
+===============  \033[m        
+
+""")
+    
+# Solicitando dados:
+logo_senai()
+for i in range (QTD):
+    numeros=int(input(f"\033[1;32m{i+1}º numero: "))
+    """numeros = random.randint(-10 , 10)#Apenas para teste rapidos."""
+    numeros_armazenados.append(numeros)
+os.system("cls||clear")
+
+#Processando daddos:
+
+def impar_par (a):
+    lista_impar= []
+    lista_par =[]
+    for numeros in a:
+        if numeros % 2 == 0:
+            lista_par.append(numeros)
+        else:
+            lista_impar.append(numeros)
+    return lista_impar , lista_par
+impares,pares = impar_par(numeros_armazenados)
+
+def positivo_negativo(a):
+    lista_negativo = []
+    lista_positivo = []
+    for numeros in a :
+        if numeros < 0 :
+            lista_negativo.append(numeros)
+        else:
+            lista_positivo.append(numeros)
+    return lista_positivo, lista_negativo
+
+positivo, negativo = positivo_negativo(numeros_armazenados)
+
+def media (a):
+    qtd = len(a)
+    soma = sum(a)
+    for numero in a:
+        if  numero!=0:
+            media = soma/qtd
+            return media
+        else:
+            return 0
+
+
+media_total = media(numeros_armazenados)
+media_impares=media(impares)
+media_pares=media(pares)
+maior = max(numeros_armazenados)
+menor = min(numeros_armazenados)
+logo_senai()
+print("\n\033[4;37m======= QUANTIDADE ========\033[m\n")
+print(f"\033[1;30mA quantidade de número pares foram de {len(pares)}")
+print(f"A quantidade de número ímpares foram de {len(impares)}")
+print(f"A quantidade de número positivos foram de {len(positivo)}")
+print(f"A quantidade de número negativos foram de {len(negativo)}")
+print("\n\033[4;37m======= MAIOR E MENOR ========\033[m\n")
+print(f"\033[1;30mA O maior número é: {maior}")
+print(f" O menor número é: {menor}")
+print("\n\033[4;37m======= MÉDIA ========\033[m\n")
+print(f"\033[1;30mA média dos números pares foram {media_pares}")
+print(f"A média dos números impares foram {media_impares:}")
+print(f"A média dos números totais foram {media_total:}")
+
+print("\n\033[4;37m======= ORDEM REVERSA ========\033[m\n")
+
+for i, numeros_inseridos in enumerate (reversed(numeros_armazenados)):
+   print(f"\033[1;30m{len(numeros_armazenados) - i}° numero: {numeros_inseridos}")
--- Mundo 2 Python/Leitura de numeros.py
+++ Mundo 2 Python/Leitura de numeros.py
@@ -14,9 +14,11 @@ list_num = []
 
 #Verificação:
 
-for i in range(QTD):
+while True:
     num = random.randint(-10,10)
     list_num.append(num)
+    if num == 0:
+        break
     
 def verificação_pares_impar (a):
     quantidade_pares = 0
@@ -27,7 +29,7 @@ def verificação_pares_impar (a):
         if numero %2==0:
             quantidade_pares +=1
             list_par.append(numero)
-        elif numero%2!=0:
+        else:
             quantidade_impares +=1
             list_impar.append(numero)
     return quantidade_pares, quantidade_impares
@@ -35,23 +37,30 @@ def verificação_pares_impar (a):
 def verificação_nega_posi(a):
     quantidade_nega=0
     quantidade_posi=0
+    qtd_posi_par = 0
     for numero in a:
-        if numero >= 0:
+        if numero > 0:
             quantidade_posi+=1
+            if numero %2 ==0:
+                qtd_posi_par+=1
         else:
             quantidade_nega+=1
-    return quantidade_posi, quantidade_nega
+    quantidade_nega+=-1    
+    return quantidade_posi, quantidade_nega,qtd_posi_par
 
-qtd_posi, qtd_nega = verificação_nega_posi(list_num)
+qtd_posi, qtd_nega,qtd_par_posi = verificação_nega_posi(list_num)
 ''
-qtd_impar, qtd_par = verificação_pares_impar(list_num)
-
+qtd_par, qtd_impar = verificação_pares_impar(list_num)
+quantidade = len(list_num)
 #Imprimindo resultado:
 
-print(f"""\033[31mPares: {qtd_par}
-Impares: {qtd_impar}
+print(f"""\033[31mImpares: {qtd_impar}
+Pares: {qtd_par}
 Positivos: {qtd_posi}
-Negativos: {qtd_nega}\n\033[m""")
+Negativos: {qtd_nega}
+Positivos pares: {qtd_par_posi}
+Quantidade de numeros inseridos: {quantidade}
+\n\033[m""")
 
 for i,numero in enumerate(list_num):
     print(f"{i+1}º Numero: {numero}")
