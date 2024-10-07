2658cced2a62a4e395707ec0c02a20920f59830f
LeonDev633 <leonardo.dev667622969@gmail.com>
2024-10-07 15:23:12 +0000

    Salvar

--- Mundo 2 Python/Games.py
+++ Mundo 2 Python/Games.py
@@ -1,44 +1,50 @@
-import random
+from random import randint
 import os
 import time
 
 os.system("cls||clear")
 
-
+derrotas = 0
+vitoria = 0
 while True:
-    while True:
-        num=random.randint(1,10)
-        derrotas = 0
-        vitoria = 0
-        tentativa=0
-        for i in range(3):
-            print("="*40)
-            print(f"{"ACERTE O NUMERO":^40}")
-            print("="*40)
-            print(f"{"Derrotas: ":^15}{derrotas}",end=" ")
-            print(f"{"Vitorias: ":^15}{vitoria}")
-            print("="*40)
-            print(f"{"DICA":^40}")
-            print(f"{"EU PENSEI EM UM NUMERO ENTRE 1 E 10":^40}")
-            print("="*40)
-            
-            tentativa = i+1
-            numero=int(input(f"{tentativa}ª Tentativa: "))
-            if numero == num:
-                print("\033[2;35mVocê acertou !\033[m")
-                vitoria +=1
-                time.sleep(2)
-                os.system("cls||clear")
-                break
-            elif numero != num:
-                print("Você errou, tente novamente.")
-                time.sleep(1)
-                os.system("cls||clear")
-                if  tentativa == 3:
-                    derrotas+=1
-                    print("="*40)
-                    print(f"{"Você usou o numero maximo de tentativas":^40}")
-                    print("="*40)
-                    os.system("cls||clear")
-                    
+    print("=-"*20)
+    print(f"{"ACERTE O NUMERO":^40}")
+    print("=-"*20)
+    print("="*40)
+    print(f"{"DICA":^40}")
+    print(f"{"EU PENSEI EM UM NUMERO ENTRE 1 E 10":^40}")
+    print("="*40)
+    tentativas = 0
+    python = randint(1,10)
+    for i in range (3):
+        tentativas += 1
+        print("="*40)
+        print(f"{f"VOCÊ TEM {(i-3)*-1} TENTATIVAS RESTANTES":^40}")
+        print("="*40)
+        num = int(input("Qual numero eu pensei ? "))
+        if num == python:
+            print("*"*40)
+            print(f"{f"VOCÊ ACERTOU, PARABÉNS!":^40}")
+            vitoria += 1
+            print("*"*40)
+            time.sleep(1)
+            break
+        elif tentativas == 3:
+            derrotas += 1
+            print(f"{"VOCÊ NÃO CONSEGUIU ACERTAR":^40}")
+            print(f"{f"O NUMERO QUE EU PENSEI FOI {python}":^40}")
+            time.sleep(1)
+            break
+    os.system("cls||clear")
+    print("=-"*20)
+    print(f"{"Derrotas: ":^15}{derrotas}",end=" ")
+    print(f"{"Vitorias: ":^15}{vitoria}")
+    print("="*40)
+    time.sleep(2)
+        
+        
+        
+    
+ 
+        
                     
